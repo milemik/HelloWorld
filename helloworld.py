4e0cdf025ec293e7775db755c22e3392acb603b1
milemik <39780627+milemik@users.noreply.github.com>
2018-05-31 00:24:56 +0200

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+# if you use linux you can do this with vim editor or any oather editor (nano) and you need to add this to the top of the code
+#if you use pyhton 2.* use !#/usr/bin/python and for 3.* next code
+
+#start python3 code
+!#/usr/bin/python3
+
+#print: Hello World
+print ('Hello World')
